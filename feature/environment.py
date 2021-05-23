a8f6f940bbeb983dac93ad1c618f2448d07555bc
SafvanD <36831861+SafvanD@users.noreply.github.com>
2021-05-23 13:52:40 -0400

    Delete feature directory

--- feature/environment.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver import chrome
-
-def before_scenario(context, scenario):
-    path = 'C:\\Users\\B\Downloads\\chromedriver_win32.chromedriver.exe'
-    context.driver = chrome(executable_path=path)
-
-def after_scenario(context, scenario):
-    context.driver.close()
